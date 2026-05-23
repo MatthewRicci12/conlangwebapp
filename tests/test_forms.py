2f9e98f2a7c76ee1fe50e09a762e1b5ef657dbdb
Matthew Ricci <matthewricci552@gmail.com>
2026-05-23 15:09:29 -0700

    Made skeleton of forms I'd like to test

---
+++ tests/test_forms.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+class UserFormTest(TestCase):
+    def setUp(self):
+        pass
+
+class SubmitTextFormTest(TestCase):
+    def setUp(self):
+        pass
+
+class UploadFileFormTest(TestCase):
+    def setUp(self):
+        pass
+
+class VocabularyEntryFormTest(TestCase):
+    def setUp(self):
+        pass
+
+class GlyphFormTest(TestCase):
+    def setUp(self):
+        pass
+
+class GrammarNoteFormTest(TestCase):
+    def setUp(self):
+        pass
