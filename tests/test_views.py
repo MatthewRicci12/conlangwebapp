0aa31e8ab361940bb63a287d4ef9c882eae85094
Matthew Ricci <matthewricci552@gmail.com>
2026-05-23 08:28:49 -0700

    Scaffolded testcase class for each view

---
+++ tests/test_views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
+class IndexViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class HandleFileViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class SubmitTokenViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class EnterTextScreenViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class UserClicksTextViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class VocabularyListViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class PhonologyAndGlyphsTabViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class GrammarTabViewTest(TestCase):
+    def setUp(self):
+        pass
+
+
+class ModalViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class LogInViewTest(TestCase):
+    def setUp(self):
+        pass
+
+class SignUpViewTest(TestCase):
+    def setUp(self):
+        pass
+
+
