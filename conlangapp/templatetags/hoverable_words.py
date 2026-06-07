aff7eee343f797d6c98a0df5d5071caf6b9fc30f
Matthew Ricci <matthewricci552@gmail.com>
2026-06-07 10:49:45 -0700

    Applied universal cool style to edit forms

--- conlangapp/templatetags/hoverable_words.py
+++ conlangapp/templatetags/hoverable_words.py
@@ -7,14 +7,10 @@ register = template.Library()
 
 @register.filter
 def hoverable_words(text, params):
-    text_id = params.get('text_id')
-    #selected_form = params.get('selected_form')
-    words = re.split(r'(\s+)', text)  # preserves whitespace
+    words = re.split(r'(\s+)', text)
     result = ''
     for token in words:
         if token.strip():
-            #url = reverse('user-clicks-text', args=[text_id])
-            #result += f'<span class="word" hx-post="{url}" hx-target="body" hx-vals=\'{{ "form_up": 1, "token": "{token}", "selected_form": "{selected_form}" }}\'>{token}</span>'
             result += f'<span class="word"}}\'>{token}</span>'
         else:
             result += token
